e48120cb5d6dd04fcd99d2ecd286b5febadae7db
a-bratosin <151036661+a-bratosin@users.noreply.github.com>
2024-04-30 01:29:04 +0300

    Downloaded the 'punkt' and 'stopwords' packages for NLTK, and added the "nltk_data" directory to the program's data path

--- TextProcessingLayer/TextProcessingLayer.py
+++ TextProcessingLayer/TextProcessingLayer.py
@@ -1,8 +1,15 @@
 import random
 import nltk
-answers = ["Hello again", "Howdy", "Go away", "The agony"]
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+# adaug la lista de date subfolderul din directorul proiectului, ca să am toate datele pre-descărcate
+(nltk.data.path).insert(0,"nltk_data")
+print(nltk.data.path)
+
 
 # Main function accessed by interface layer
 def text_processing_layer(question: str):
-    x = random.randint(0, 3)
-    return answers[x]
+    nltk.download('stopwords')
+    
