f4f15f67a25f8c3d6f287a4188da83b2a065cca3
KiwiKing2k <73382849+KiwiKing2k@users.noreply.github.com>
2024-05-03 13:46:02 +0300

    Add files via upload

---
+++ gabi-patrascu-nu-ma-uita.h
@@ -0,0 +1,13 @@
+//
+// Created by dinus on 4/30/2024.
+//
+
+#ifndef CHATBOT_GABI_PATRASCU_NU_MA_UITA_H
+
+long int gabi_hashuieste_pls(int important, int paragraph){
+    //face cv hash cu info
+}
+
+#define CHATBOT_GABI_PATRASCU_NU_MA_UITA_H
+
+#endif //CHATBOT_GABI_PATRASCU_NU_MA_UITA_H
