c72dc95b155cfc807da7d7cae3303d39240c902d
KiwiKing2k <dinustefan04@gmail.com>
2024-05-05 18:16:59 +0300

    Environment variables

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ChatBot3")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ChatBot")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
