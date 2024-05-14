047beb90f7b14b97faf1c701fe550a9d65ca04b4
Patraxbi <patraxbi@outlook.com>
2024-05-15 01:04:39 +0300

    resolved bug with bitwise encoding changed bitwise to unsigned long for safety ( int is only 16 bits on some machines)

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ## bitwise copression - decompression of information
 
+### type introduced
+
+typedef unsigned long bitwise 
 ### void encode_info(bitwise *encoded, bitwise importance, bitwise module, bitwise paragraph);
 <p>is used to compress the information <br>
 encoded - the adress where the function will save the information <br>
--- bitwise_encoding.c
+++ bitwise_encoding.c
@@ -2,13 +2,21 @@
 #include <stdio.h>
 
 /*
-this works perfectly
 int main(void){
 
-    bitwise encoded, importance = 2, module = 3, paragraph= 28;
-    encode_info(&encoded, importance, module, paragraph);
-    get_info(encoded, &importance, &module, &paragraph);
-    printf("importance= %u \n module= %u \n paragraph= %u \n", importance, module, paragraph);
+    bitwise encoded, importance, module , paragraph;
+	bitwise outimportance, outmodule, outparagraph;
+	for ( importance = 0; importance < 5; importance++)
+		for( module = 0; module< 7; module++)
+			for (paragraph = 0; paragraph < 1000; paragraph++){
+    			encode_info(&encoded, importance, module, paragraph);
+    			get_info(encoded, &outimportance, &outmodule, &outparagraph);
+					if( importance != outimportance || module != outmodule || paragraph != outparagraph){
+    			printf("\n   importance= %lu\t   module= %lu\t   paragraph= %lu \n", importance, module, paragraph);
+				printf("outimportance= %lu\toutmodule= %lu\toutparagraph= %lu \n", outimportance, outmodule, outparagraph);
+
+					}
+	}
 }
 */
 
@@ -28,7 +36,7 @@ int encode(bitwise *encoded, bitwise bit_start, bitwise bit_end, bitwise info)
 
 	// checking if info is small enough to get in bit_end - bit_start + 1 bits
 	bitwise auxiliar = 1;
-	auxiliar = auxiliar << (bit_end - bit_start + 2);
+	auxiliar = auxiliar << (bit_end - bit_start + 1);
 	auxiliar--;
 
 	if (auxiliar < info)
@@ -52,7 +60,11 @@ bitwise decode(bitwise encoded, bitwise bit_start, bitwise bit_end){
     encoded = encoded >> bit_start; // deleting first bits
 
     bitwise auxiliar = 1;
-    auxiliar = auxiliar << bit_end -bit_start +2; // creating a mask for specified bits
+    auxiliar = auxiliar << bit_end -bit_start +1; // creating a mask for specified bits
+									// previously was +2 because I forgot 1 is a bit on itself
+	// formula is (bit_end - bitstart +1) - bits for the mask
+	// + 1 , because u need one more bit for then to decrement and all to be 1s
+	// -1, because we start form auxiliar=1, which already has bit on pos 0
     auxiliar--;
     return (encoded & auxiliar); // getting information
 }
@@ -60,5 +72,5 @@ bitwise decode(bitwise encoded, bitwise bit_start, bitwise bit_end){
 void get_info( bitwise encoded, bitwise *importance, bitwise *module, bitwise *paragraph){
     *importance = decode(encoded, PARAGRAPH_BITS + MODULE_BITS , PARAGRAPH_BITS + MODULE_BITS + IMPORTANCE_BITS-1);
 	*module = decode(encoded, PARAGRAPH_BITS , PARAGRAPH_BITS + MODULE_BITS-1);
-	*paragraph = (encoded, 0, PARAGRAPH_BITS-1);
+	*paragraph = decode(encoded, 0, PARAGRAPH_BITS-1);
 }
